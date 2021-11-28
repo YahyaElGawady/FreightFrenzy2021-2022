53bf9fb9fca2069ac8b3db91450ad1ed1d42b21d
tycebrown <tycebrown247@gmail.com>
2021-11-28 15:52:25 -0600

    Update DuckeySpinner.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzyComponents/DuckeySpinner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzyComponents/DuckeySpinner.java
@@ -1,25 +1,33 @@
 package org.firstinspires.ftc.teamcode.FreightFrenzyComponents;
 
-import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.CRServo;
 
 import org.firstinspires.ftc.robotcontroller.internal.RobotBase;
 import org.firstinspires.ftc.robotcontroller.internal.RobotComponent;
 
 public class DuckeySpinner extends RobotComponent {
-    public Servo spinner;
+    public CRServo spinner;
+    public boolean isButtonHeld = false;
+
     public static final double moveCount = 0; // TODO: fill with accurate number
+    public static final double SPINPOWER = 0; // TODO: fill with proper power
 
     public DuckeySpinner(RobotBase base){
         super(base);
-        spinner = base.getMapper().mapServo("duckeySpinner");
+        spinner = base.getMapper().mapCRServo("duckeySpinner");
     }
 
     public void spin(boolean button){
-        if(button){
-            spinner.setPosition((spinner.getPosition() + moveCount) % 1);
+        if(button && !isButtonHeld){
+            isButtonHeld = true;
+            spinner.setPower(SPINPOWER);
+        }
+        else if(!button){
+            isButtonHeld = false;
+            spinner.setPower(0);
         }
     }
 
     @Override
-    public void stop() {}
+    public void stop() { spinner.setPower(0);}
 }
