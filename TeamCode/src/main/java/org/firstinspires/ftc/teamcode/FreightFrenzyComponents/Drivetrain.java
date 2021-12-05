12e474cc5e56e1325347dfe52123e7b70890015f
tycebrown <tycebrown247@gmail.com>
2021-12-05 16:05:37 -0600

    Update Drivetrain.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzyComponents/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzyComponents/Drivetrain.java
@@ -25,10 +25,10 @@ public class Drivetrain extends RobotComponent {
 
     public final double STRAIGHT = 0;
 
-
-    static final double     COUNTS_PER_MOTOR_REV    = 753.2;
-    static final double     DRIVE_GEAR_REDUCTION    = 1.0;
-    static final double     WHEEL_DIAMETER_INCHES   = 3.77953;
+    public static final double amountError = 2;
+    public static final double     COUNTS_PER_MOTOR_REV    = 753.2;
+    public static final double     DRIVE_GEAR_REDUCTION    = 1.0;
+    public static final double     WHEEL_DIAMETER_INCHES   = /*3.77953*/3.75;
     public static final double     COUNTS_PER_INCH  = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION)
             / (WHEEL_DIAMETER_INCHES * 3.14159265358979323846264);
     public static final double DRIVE_SPEED = 0.65;     // Nominal speed for better accuracy.
@@ -58,6 +58,81 @@ public class Drivetrain extends RobotComponent {
         setZeroPowerBehaviors(DcMotor.ZeroPowerBehavior.BRAKE);
 
      }
+    public void moveInches(double speed,
+        double frontLeftInches, double frontRightInches, double backLeftInches,
+        double backRightInches)
+    {
+        int newFrontLeftTarget;
+        int newFrontRightTarget;
+        int newBackLeftTarget;
+        int newBackRightTarget;
+        boolean goodEnough = false;
+        double ErrorAmount;
+        // Ensure that the opmode is still active
+        if (base.getOpMode().opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newFrontLeftTarget = frontLeft.getCurrentPosition() + (int) (frontLeftInches * COUNTS_PER_INCH);
+            newFrontRightTarget = frontRight.getCurrentPosition() + (int) (frontRightInches * COUNTS_PER_INCH);
+            newBackLeftTarget = backLeft.getCurrentPosition() + (int) (backLeftInches * COUNTS_PER_INCH);
+            newBackRightTarget = backRight.getCurrentPosition() + (int) (backRightInches * COUNTS_PER_INCH);
+
+            setTargetPositions(newFrontLeftTarget, newFrontRightTarget,
+                    newBackLeftTarget, newBackRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            setModes(DcMotor.RunMode.RUN_TO_POSITION);
+
+            setPowers(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            // Note: We use (isBusy() && isBusy()) in the loop test, which means that when EITHER motor hits
+            // its target position, the motion will stop.  This is "safer" in the event that the robot will
+            // always end the motion as soon as possible.
+            // However, if you require that BOTH motors have finished their moves before the robot continues
+            // onto the next step, use (isBusy() || isBusy()) in the loop test.
+            while (base().getOpMode().opModeIsActive() &&
+                    frontLeft.isBusy() && frontRight.isBusy() &&
+                    backLeft.isBusy() && backRight.isBusy() && !goodEnough) {
+
+                // Display it for the driver.
+                base().getTelemetry().addData("Path1", "Running to %7d :%7d", newFrontLeftTarget, newBackLeftTarget, newFrontRightTarget, newBackRightTarget);
+                base().getTelemetry().addData("Path2", "Running at %7d :%7d",
+
+                        frontLeft.getCurrentPosition(),
+                        frontRight.getCurrentPosition(),
+                        backLeft.getCurrentPosition(),
+                        backRight.getCurrentPosition());
+                base().getTelemetry().addData("frontLeft", frontLeft.getCurrentPosition());
+                base().getTelemetry().addData("backLeft", backLeft.getCurrentPosition());
+                base().getTelemetry().addData("frontRight", frontRight.getCurrentPosition());
+                base().getTelemetry().addData("backright", backRight.getCurrentPosition());
+
+                base().getTelemetry().update();
+
+                ErrorAmount = ((Math.abs(((newBackLeftTarget) - (backLeft.getCurrentPosition())))
+                        + (Math.abs(((newFrontLeftTarget) - (frontLeft.getCurrentPosition()))))
+                        + (Math.abs((newBackRightTarget) - (backRight.getCurrentPosition())))
+                        + (Math.abs(((newFrontRightTarget) - (frontRight.getCurrentPosition()))))) / COUNTS_PER_INCH);
+                if (ErrorAmount < amountError) {
+                    goodEnough = true;
+                }
+            }
+
+            // Stop all motion;
+
+            frontLeft.setPower(0);
+            frontRight.setPower(0);
+            backLeft.setPower(0);
+            backRight.setPower(0);
+
+            // Turn off RUN_TO_POSITION
+            frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+    }
     public void drive(double forward, double turn, boolean slowMode) {
 
         forward = getProcessedInput(forward);
@@ -121,7 +196,12 @@ public class Drivetrain extends RobotComponent {
             return (sum / (double) (dcmotors.size()));
         }
 
-
+        public void setTargetPositions(int fl, int fr, int bl, int br) {
+            frontLeft.setTargetPosition(fl);
+            frontRight.setTargetPosition(fr);
+            backLeft.setTargetPosition(bl);
+            backRight.setTargetPosition(br);
+        }
         public void stop () {
             setPowers(0);
         }
