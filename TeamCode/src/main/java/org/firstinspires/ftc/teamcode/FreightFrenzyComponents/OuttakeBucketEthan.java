bf62cabb00f06baf57474da6b42eda5d656c4576
EthanB2 <93627483+EthanB2@users.noreply.github.com>
2021-12-05 15:16:12 -0600

    Changese to ethan's outtake bucket and main teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzyComponents/OuttakeBucketEthan.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FreightFrenzyComponents/OuttakeBucketEthan.java
@@ -13,6 +13,16 @@ public class OuttakeBucketEthan extends RobotComponent {
 
     boolean buttonIsHeld  = false;
     boolean positionIn = true;
+    boolean dpadUpIsPressed = false;
+    boolean dpadDownIsPressed = false;
+    //todo change values to correct values
+    public final int Top = 100;
+    public final int Middle = 50;
+    public final int Bottom = 0;
+    public int currentSelectedPosition = Bottom;
+    boolean buttonAIsPressed = false;
+    boolean buttonAToggle = false;
+
 
     public OuttakeBucketEthan(RobotBase base) {
         super(base);
@@ -38,4 +48,35 @@ public class OuttakeBucketEthan extends RobotComponent {
     public void stop() {
 
     }
+
+    public void SelectedSliderPosition(boolean dpadUp, boolean dpadDown) {
+        if(dpadUp && !dpadUpIsPressed); {
+            switch(currentSelectedPosition) {
+                case Bottom: currentSelectedPosition = Middle;break;
+                case Middle: currentSelectedPosition = Top;
+            }
+        if(dpadDown && !dpadDownIsPressed); {
+            switch(currentSelectedPosition) {
+                case Middle: currentSelectedPosition = Bottom; break;
+                case Top: currentSelectedPosition = Middle;
+            }
+            }
+            dpadUpIsPressed = dpadUp;
+            dpadDownIsPressed = dpadDown;
+    }
+
+}
+    public void SliderPosition(boolean buttonA) {
+        if(buttonA && !buttonAIsPressed) {
+            buttonAToggle = !buttonAToggle;
+            if(buttonAToggle){
+                slider.setTargetPosition(currentSelectedPosition);
+            }
+            else{
+                slider.setTargetPosition(Bottom);
+            }
+        }
+        buttonAIsPressed = buttonA;
+    }
+
 }
